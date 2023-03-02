35f081108df5e0ad692a295f62f506489feab797
Janell Huyck <Janell.Huyck@outlook.com>
2023-03-02 16:10:40 -0500

    Cart icon counts items inside

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import './cart-icon.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, itemCount } = useContext(CartContext);
 
     const toggleCart = () => {
         setIsCartOpen(!isCartOpen);
@@ -14,7 +14,7 @@ const CartIcon = () => {
     return (
         <div className="cart-icon-container" onClick={toggleCart}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     );
     };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+
 
 const addCartItem = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
@@ -22,24 +24,29 @@ export const CartContext = createContext({
     cartItems: [],
     setCartItems: () => {},
     addItemToCart: () => {},
+    itemCount: 0,
    });
 
 export const CartProvider = ({ children }) => {
    const [isCartOpen, setIsCartOpen] = useState(false);
    const [cartItems, setCartItems] = useState([]);
+   const [itemCount, setItemCount] = useState(0);
 
    const addItemToCart = (cartItemToAdd) => {
         const newCartItems = addCartItem(cartItems, cartItemToAdd);
         setCartItems(newCartItems);
    };
 
-    
+   useEffect(() => {
+    setItemCount(cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0));
+    }, [cartItems]);
     
     const value = {
         isCartOpen,
         setIsCartOpen,
         addItemToCart,
         cartItems,
+        itemCount
     };
 
    return (
